3acffd4ba28739bedcca21edf1d32334b16bd125
2101class <2171489535@qq.com>
2021-08-24 21:18:18 +0800

    这是第一次提交 1.0

---
+++ src/main/java/cn/baizhi/GitTest1Application.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/baizhi/test/test1.java
@@ -0,0 +1,13 @@
+package cn.baizhi.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class test1 {
+    @Test
+    public void  t()
+    {
+        System.out.println(11);
+    }
+}
